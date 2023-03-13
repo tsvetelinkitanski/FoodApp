48321046431a557a1857c37553d3b9094ef71b73
Tsvetelin <kitanskitsvetelin@gmail.com>
2023-03-13 19:48:21 +0200

    Navigation style

--- components/Details.js
+++ components/Details.js
@@ -1,15 +1,35 @@
 import * as React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import Feather from "react-native-vector-icons/Feather";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import colors from "../assets/colors";
 
-export default Details = ({ route }) => {
+export default Details = ({ route, navigation }) => {
   const { item } = route.params;
   console.log(item);
   return (
     <View style={styles.container}>
       {/* Header */}
       <SafeAreaView>
-        <View>
-          <Text>Some text here</Text>
+        <View style={styles.headerWrapper}>
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <View style={styles.headerLeft}>
+              <Feather name="chevron-left" size={12} color={colors.te} />
+            </View>
+          </TouchableOpacity>
+          <View style={styles.headerRight}>
+            <MaterialCommunityIcons
+              name="star"
+              size={12}
+              color={colors.background}
+            />
+          </View>
         </View>
       </SafeAreaView>
     </View>
@@ -19,5 +39,25 @@ export default Details = ({ route }) => {
 const styles = new StyleSheet.create({
   container: {
     flex: 1,
+  },
+  headerWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  headerLeft: {
+    borderColor: colors.textGray,
+    borderWidth: 2,
+    padding: 12,
+    borderRadius: 10,
+  },
+  headerRight: {
+    backgroundColor: colors.primary,
+    borderWidth: 2,
+    padding: 12,
+    borderRadius: 10,
+    borderColor: colors.primary,
   },
 });
